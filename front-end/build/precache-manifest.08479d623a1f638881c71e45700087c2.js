efd0dd9cf4a62ae569e59217acf3c11c892d6725
Jack <jack@Jacks-MacBook-Air.local>
2019-02-13 19:06:53 -0500

    come on

---
+++ front-end/build/precache-manifest.08479d623a1f638881c71e45700087c2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e8f2b22657386d4509",
+    "url": "/static/css/main.d9defce4.chunk.css"
+  },
+  {
+    "revision": "66e8f2b22657386d4509",
+    "url": "/static/js/main.66e8f2b2.chunk.js"
+  },
+  {
+    "revision": "3ed00d259f5e742ff0cb",
+    "url": "/static/js/1.3ed00d25.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3f69df26868cd09fb288578eb91a579",
+    "url": "/static/media/art_pic_blog.c3f69df2.jpg"
+  },
+  {
+    "revision": "6cfb5dacf4f033f396d733e33b9388ca",
+    "url": "/static/media/jen_pic_blog.6cfb5dac.jpg"
+  },
+  {
+    "revision": "bb0ba58074bf61486d31ae2fcf8991c3",
+    "url": "/static/media/jon_pic_blog.bb0ba580.jpg"
+  },
+  {
+    "revision": "e6898a6fdf11f61d0e46968a4d979320",
+    "url": "/static/media/blueBack.e6898a6f.png"
+  },
+  {
+    "revision": "b504c4a4880009514d4264ca83c81c62",
+    "url": "/static/media/education.b504c4a4.png"
+  },
+  {
+    "revision": "79768ed5247a22f782c628f8d340b1ca",
+    "url": "/static/media/book.79768ed5.jpg"
+  },
+  {
+    "revision": "30a84d75fd6988ee96e6c1cd2ff7a4c8",
+    "url": "/static/media/amazon.30a84d75.png"
+  },
+  {
+    "revision": "719ad77e7e56ad4f923db616741833eb",
+    "url": "/static/media/greenBack.719ad77e.png"
+  },
+  {
+    "revision": "904c1e632e95b5ddb8e91d419f590a8c",
+    "url": "/static/media/redBack.904c1e63.png"
+  },
+  {
+    "revision": "627ba1931fbc9286ab2c17caf79618f0",
+    "url": "/static/media/yellowBackOpaque.627ba193.png"
+  },
+  {
+    "revision": "167c9d24e4db542add57939cc5f0214f",
+    "url": "/static/media/yellowBack.167c9d24.png"
+  },
+  {
+    "revision": "cc7c6238c155719b1c97377bb82890e3",
+    "url": "/static/media/blog.cc7c6238.png"
+  },
+  {
+    "revision": "6aa9ec0a7ea4b51a821e29d09fb37cd3",
+    "url": "/index.html"
+  }
+];
